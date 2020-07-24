241969113a7a3a8b98728d15fddc092bc37c634b
peaky76 <robertjamespeacock@gmail.com>
2020-07-24 11:51:44 +0100

    form and delete button added for album list

--- js/app.js
+++ js/app.js
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const save = document.querySelector("#form-album-entry");
   save.addEventListener("submit", handleFormSave);
 
-  const cancel = document.querySelector("#btn-cancel");
-  cancel.addEventListener("click", handleFormCancel);
+  const cancel = document.querySelector("#form-album-entry");
+  cancel.addEventListener("reset", handleFormCancel);
+
+  const deleteAll = document.querySelector("#form-album-list");
+  deleteAll.addEventListener("reset", handleListDelete);
 });
 
 const handleFormSave = function (e) {
@@ -20,13 +23,11 @@ const handleFormSave = function (e) {
 
   const albumList = document.querySelector("#album-list");
   albumList.appendChild(newAlbum);
-
-  //   console.log(
-  //     `Form saved with title: ${title}, artist: ${artist}, rating: ${rating}`
-  //   );
 };
 
 const handleFormCancel = function (e) {
   e.preventDefault();
   console.log("Form cancelled");
 };
+
+const handleListDelete = function (e) {};
